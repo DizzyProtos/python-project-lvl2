a711f475532b781c14fef7eed6f2ee61f640b9c8
dizzyprotos <medvedcpp@gmail.com>
2022-07-30 14:56:49 +0700

    Fix correct answer check in gendiff tests

--- tests/gendiff_test.py
+++ tests/gendiff_test.py
@@ -1,5 +1,5 @@
 from sre_parse import fix_flags
-import pytest
+import os
 from gendiff.scripts.gendiff import generate_diff
 
 
@@ -7,5 +7,12 @@ def test_gendiff():
     test_file1 = 'tests/fixtures/1.json'
     test_file2 = 'tests/fixtures/2.json'
     diff = generate_diff(test_file1, test_file2)
-    correct_answer = '{\t-follow: False\n\thost: hexlet.io\n\t-proxy: 123.234.53.22\n\t-timeout: 50\n\t+timeout: 20\n\t+verbose: True\n}'
-    assert diff == correct_answer
+    with open('temp_difference.txt', 'w') as f:
+        f.write(diff)
+
+    with open('temp_difference.txt', 'r') as f:
+        answer = f.read()
+    with open(r'tests/fixtures/correct.txt', 'r') as f:
+        correct_answer = f.read()
+    assert answer == correct_answer
+    os.remove('temp_difference.txt')
