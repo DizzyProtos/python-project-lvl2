c2e706088066eb14e975e1fbd186bba3b2e5f002
dizzyprotos <medvedcpp@gmail.com>
2022-09-04 22:44:56 +0700

    change format name in tests

--- tests/gendiff_test.py
+++ tests/gendiff_test.py
@@ -2,25 +2,24 @@ import json
 from gendiff import generate_diff
 
 
-def run_test(test_file1, test_file2, correct_file, format='pretty'):
-    answer = generate_diff(test_file1, test_file2, format)
+def run_test(test_file1, test_file2, correct_file, format='stylish'):
     with open(correct_file, 'r') as f:
         correct_answer = f.read()
-    assert answer == correct_answer
+    assert generate_diff(test_file1, test_file2, format) == correct_answer
 
 
 def test_gendiff_json():
     test_file1 = 'tests/fixtures/1.json'
     test_file2 = 'tests/fixtures/2.json'
     correct_file = 'tests/fixtures/correct.txt'
-    run_test(test_file1, test_file2, correct_file, 'pretty')
+    run_test(test_file1, test_file2, correct_file, 'stylish')
 
 
 def test_gendiff_yaml():
     test_file1 = 'tests/fixtures/1.yaml'
     test_file2 = 'tests/fixtures/2.yaml'
     correct_file = 'tests/fixtures/correct.txt'
-    run_test(test_file1, test_file2, correct_file, 'pretty')
+    run_test(test_file1, test_file2, correct_file, 'stylish')
 
 
 def test_gendiff_json_plain():
