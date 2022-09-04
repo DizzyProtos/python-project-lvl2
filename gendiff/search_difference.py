661d8430a7790aa97e0ed4c56cad3545c569f08d
dizzyprotos <medvedcpp@gmail.com>
2022-09-04 22:38:15 +0700

    refactor _get_diff_at_key()

--- gendiff/search_difference.py
+++ gendiff/search_difference.py
@@ -29,17 +29,16 @@ def _get_diff_at_key(key, first_dict, second_dict):
     first_value = first_dict.get(key, None)
     second_value = second_dict.get(key, None)
     if isinstance(first_value, dict) and isinstance(second_value, dict):
-        line = ()
-    if key not in first_dict:  # added
-        line = (ADD, key, second_value)
-    elif key not in second_dict:  # removed
-        line = (REMOVE, key, first_value)
-    elif first_value == second_value:  # equal
-        line = (SAME, key, first_value)
-    elif first_value != second_value:  # changed
-        line = (UPDATE, key, (first_value, second_value))
-
-    return line
+        return ()
+    if key not in first_dict:
+        return (ADD, key, second_value)
+    if key not in second_dict:
+        return (REMOVE, key, first_value)
+    if first_value == second_value:
+        return (SAME, key, first_value)
+    if first_value != second_value:
+        return (UPDATE, key, (first_value, second_value))
+    return ()
 
 
 def get_diff(first_dict, second_dict):
